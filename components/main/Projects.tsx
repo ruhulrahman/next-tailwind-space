f67d56994f989e4678c02a1a1af3e70d40d04eae
ruhulrahman2233 <ruhulrahman2233@gmail.com>
2024-01-07 18:59:07 +0600

    menu updated

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -4,7 +4,7 @@ import ProjectCards from '../sub/ProjectCards'
 
 const Projects = () => {
   return (
-    <div className='flex flex-col items-center justify-center py-20'>
+    <div id='projects' className='flex flex-col items-center justify-center py-20'>
       <h1 className='text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20'>
         My Projects
       </h1>
